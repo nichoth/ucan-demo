6dc243290e76b63f3aa01670900302e32137175a
nichoth <nichoth@gmail.com>
2021-10-24 19:10:01 -0700

    more setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import { render } from 'preact'
 import { html } from 'htm/preact'
 
-var el = html`<div>the app</div>`
+var el = html`<div>
+    <ul class="user-list">
+        ${['alice', 'bob', 'carol'].map(name => {
+            return html`<${User} id=${1} name="${name}" />`
+        })}
+    </ul>
+</div>`
+
+function User ({ id, name }) {
+    return html`<li class="user">${name} ${id}</li>`
+}
 
 render(el, document.getElementById('content'))
