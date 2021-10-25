17538684242d7dc4f6cbf5834e74780eb10bd0f2
nichoth <nichoth@gmail.com>
2021-10-24 22:00:25 -0700

    style

--- src/index.js
+++ src/index.js
@@ -3,28 +3,45 @@ import { html } from 'htm/preact'
 import { useState, useEffect } from 'preact/hooks'
 import * as ucan from 'ucans'
 
-const users = ['alice', 'bob', 'carol']
-
 function TheApp () {
-    var [keys, setKeys] = useState([])
+    // a list like
+    // [ { name, keys } ]
+    var [users, setUsers] = useState([])
+
     useEffect(() => {
-        Promise.all(users.map(() => {
+        Promise.all(['alice', 'bob', 'carol'].map((name) => {
             return ucan.keypair.create(ucan.KeyType.Edwards)
+                .then((kp) => ({ name, keys: kp }))
         }))
-            .then(_keys => setKeys(_keys))
+            .then(_users => setUsers(_users))
     }, [])
 
     return html`<div>
+        <h1>The country club</h1>
+
+        <h2>users</h2>
         <ul class="user-list">
-            ${users.map((name, i) => {
+            ${users.map(({ name, keys }) => {
                 return html`<${User}
-                    id=${keys[i] && keys[i].did()}
+                    id=${keys && keys.did()}
                     name="${name}"
                 />`
             })}
         </ul>
     </div>`
 }
+
+
+// create an invitation is like
+// * alice is a member
+// * alice creates an invitation
+// * bob asks to redeem the invitation -- sends a message with an invitation
+//   and also bob's DID
+// * server says, 'yes this invitation is ok. i will create a UCAN for you
+//    by using my private key'
+// * bob recieves the UCAN and saves it
+// * on bob requests, the server needs to check their UCAN and verify that
+//    at the root it was signed with the server keys
 
 
 function User ({ id, name }) {
